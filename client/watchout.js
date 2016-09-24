5690ff2cb6eca2a644be8f1a8da06c963f4693e7
meijiao <meijiao@gmail.com>
2016-09-23 18:50:29 -0700

    Make player dot and implement drag

--- client/watchout.js
+++ client/watchout.js
@@ -6,14 +6,20 @@ var score = 0;
 var collisions = 0;
 var highScore = 0;
 
+//initialize colliders
+//var colliders = d3.range(20).map(function() { return [Math.random() * height, Math.random() * width]; });
+var colliders = d3.range(20).map(function() { return Math.random(); });
+var player = [{x: height / 2, y: width / 2}];
 
-var colliders = d3.range(20).map(function() { return [Math.random() * height, Math.random() * width]; });
-var player = [height / 2, width / 2];
 
+var drag = d3.behavior.drag().on('drag', function (d) {
+  // d3.select(this).attr('cx', d.x = d3.event.x).attr('cy', d.y = d3.event.y);
+  var loc = d3.mouse(this);
+  // mouse = {x: loc[0], y: loc[1]};
+  d3.select(this).attr('cx', loc[0]).attr('cy', loc[1]);
+});
 
-
-//initialize
-d3.select('body')
+d3.select('.board')
   .append('svg')
     .attr('height', height)
     .attr('width', width)
@@ -21,29 +27,46 @@ d3.select('body')
   .data(colliders)
   .enter().append('circle')
   .attr('transform', function (d) { return 'translate(' + d + ')'; })
+  // .attr('cx', Math.random() * height)
+  // .attr('cy', Math.random() * width)
   .attr('r', 24)
+  .attr('class', 'enemy')
   .style('fill', d3.rgb(10, 10, 210));
 
-d3.select('body')
-  .append('svg')
-    .attr('height', 20)
-    .attr('width', 20)
+// initalize player
+d3.select('svg')
+  // .append('svg')
+  //   .attr('height', 100)
+  //   .attr('width', 100)
   .selectAll('circle')
-    .data(player)
+  .data(colliders.concat(player))
   .enter().append('circle')
-  .attr('transform', function(d) { return 'translate(' + d + ')'; })
-  .attr('r', 16)
-  .style('fill', d3.rgb(210, 10, 10));
+  .attr('cx', function(d) { return d.x; })
+  .attr('cy', function(d) { return d.y; })
+  .attr('r', 24)
+  .attr('class', 'player')
+  .style('fill', d3.rgb(210, 10, 10))
+  .call(drag);
+
+
+// var clicked = function () {
+//   console.log('clicked');
+// };
+
+// d3.select('player')
+//   .call(drag);
+
+
+ 
 
 //randomize dots at set interval
 setInterval(function() {
-  var colliders = d3.range(20).map(function() { return [Math.random() * height, Math.random() * width] });
+  var colliders = d3.range(20).map(function() { return [Math.random() * height, Math.random() * width]; });
   d3.select('body')
   .selectAll('circle')
   .data(colliders)
   .transition().duration(2500)
   .attr('transform', function (d) { return 'translate(' + d + ')'; });  
-
 
 }, 2500);
 
@@ -52,4 +75,5 @@ setInterval(function() {
   d3.select('body')
     .select('.current')
     .text('Current score: ' + score);
-}, 50);
+}, 100);
+
