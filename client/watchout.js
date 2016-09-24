934c8d9e61e955ae97d18deff947a29f4c8f59fe
meijiao <meijiao@gmail.com>
2016-09-24 16:28:17 -0700

    Change enemies to planet gifs and Fix drag event on player

--- client/watchout.js
+++ client/watchout.js
@@ -7,9 +7,8 @@ var collisions = 0;
 var highScore = 0;
 
 //initialize colliders
-var colliders = d3.range(20).map(function() { return [Math.random() * height, Math.random() * width]; });
-//var colliders = d3.range(20).map(function() { return Math.random(); });
-var player = [{x: height / 2, y: width / 2}];
+var colliders = d3.range(15).map(function() { return [Math.random() * height, Math.random() * width]; });
+var player = [[height / 2, width / 2]];
 
 
 var collide = function () {
@@ -34,48 +33,63 @@ var collide = function () {
     .select('.highscore')
     .text('High score: ' + highScore);
 };
-var drag = d3.behavior.drag().on('drag', function (d) {
-  // d3.select(this).attr('cx', d.x = d3.event.x).attr('cy', d.y = d3.event.y);
-  var loc = d3.mouse(this);
-  // mouse = {x: loc[0], y: loc[1]};
-  d3.select(this).attr('cx', loc[0]).attr('cy', loc[1]);
-});
+
+var pixels = function(str){
+  return '' + Number(str) + 'px';
+};
+
+
+var move = function() {
+  console.log('hi');
+  d3.select('.player')
+    .attr('x', d3.event.x - parseInt(d3.select('.player').attr('width')) / 2)
+    .attr('y', d3.event.y - parseInt(d3.select('.player').attr('height')) / 2);
+};
+
+var drag = d3.behavior.drag().on('drag', move);
 
 d3.select('.board')
   .append('svg')
     .attr('height', height)
-    .attr('width', width)
-  .selectAll('circle')
+    .attr('width', width);
+  
+d3.select('svg')
+  .selectAll('.enemy')
   .data(colliders)
-  .enter().append('circle')
-  .attr('cx', 0)
-  .attr('cy', 0)
+  .enter()
+  .append('image')
+  .attr ('xlink:href', 'https://media.giphy.com/media/yo1whaKkz38ME/giphy.gif')
+  .attr('height', 100)
+  .attr('width', 100)
   .attr('transform', function (d) { return 'translate(' + d + ')'; })
-  .attr('r', 24)
-  .attr('class', 'enemy')
-  .style('fill', d3.rgb(10, 10, 210));
+  .attr('class', 'enemy');
 
 // initalize player
 d3.select('svg')
-  // .append('svg')
-  //   .attr('height', 100)
-  //   .attr('width', 100)
-  .selectAll('circle')
-  .data(colliders.concat(player))
-  .enter().append('circle')
-  .attr('cx', function(d) { return d.x; })
-  .attr('cy', function(d) { return d.y; })
-  .attr('r', 24)
+  .selectAll('.player')
+  .data(player)
+  .enter()
+  .append('image')
+  .attr('xlink:href', 'paths.svg')
+  // .attr('transform', function (d) { return 'translate(' + d + ')'; })
+  // .attr('cx', function(d) { return d[0]; })
+  // .attr('cy', function(d) { return d[1]; })
+  .attr('height', 50)
+  .attr('width', 50)
+  .attr('x', height / 2)
+  .attr('y', width / 2)
+  .attr('xlink:href', 'asteroid.png')
   .attr('class', 'player')
-  .style('fill', d3.rgb(210, 10, 10))
+  // .style('fill', d3.rgb(210, 10, 10))
   .call(drag);
- 
+
+
 var colliding = false;
 //randomize dots at set interval
 setInterval(function() {
-  var colliders = d3.range(20).map(function() { return [Math.random() * width, Math.random() * height]; });
+  var colliders = d3.range(15).map(function() { return [Math.random() * width, Math.random() * height]; });
   d3.select('body')
-  .selectAll('circle')
+  .selectAll('.enemy')
   .data(colliders)
   .transition().duration(2500)
   .attr('transform', function (d) { return 'translate(' + d + ')'; });  
@@ -103,14 +117,14 @@ setInterval(function() {
   d3.selectAll('.enemy').each(function(d) {
     enemies.push(d3.select(this).attr('transform'));
   });
-  var playerloc = [d3.select('.player').attr('cx'), d3.select('.player').attr('cy')]; 
+  var playerloc = [Number(d3.select('.player').attr('x')) + 25, Number(d3.select('.player').attr('y')) + 25]; 
   var coordinates = enemies.map(function(translate) { return translate.slice(10).split(','); });
   
   coordinates.forEach(function(coordinate) {
-    var x = Number(coordinate[0].slice(0,5)) - Number(playerloc[0]);
-    var y = Number(coordinate[1].slice(0,5)) - Number(playerloc[1]);
+    var x = Number(coordinate[0].slice(0,5)) + 75 - playerloc[0];
+    var y = Number(coordinate[1].slice(0,5)) + 75 - playerloc[1];
     
-    if (Math.abs(x) < 48 && Math.abs(y) < 48) {
+    if (Math.abs(x) < 75 && Math.abs(y) < 75) {
       if (colliding === false){
         collide();
       }
@@ -121,7 +135,7 @@ setInterval(function() {
   });
 
 
-}, 500);
+}, 100);
 
 
 
